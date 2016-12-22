5613130247165e1f4170643e5ad6a990f1cd6c28
Rasmus Christiansen <rall94.thekaas@gmail.com>
2016-12-22 21:33:01 +0100

    numberwizard UI

---
+++ numberwizardUI/Assets/Scripts/NumberWizards.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class NumberWizards : MonoBehaviour {
+	// Use this for initialization
+	int max;
+	int min;
+	int guess;
+    int maxGuesses = 10;
+
+    public Text text;
+	
+	void Start () {
+		StartGame();
+	}
+
+    void Update()
+    {
+        text.text = "My guess is " + guess;
+    }
+	
+	void StartGame () {
+		max = 1001;
+		min = 1;
+		guess = Random.Range(min, max);
+		
+	}
+
+    public void GuessHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void GuessLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+
+	void NextGuess () {
+        guess = Random.Range(min, max);
+        maxGuesses--;
+        if (maxGuesses <= 0)
+            SceneManager.LoadScene("win");
+	}
+}
